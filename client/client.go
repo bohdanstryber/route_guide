648291dc4672b502648a22769fa5723ab2d6dcbf
bohdanpodlisetskyi <bohdan@stryber.com>
2021-07-13 11:09:34 +0300

    server and client

---
+++ client/client.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io"
+	"log"
+	"time"
+
+	pb "github.com/bohdanstryber/route_guide/routeguide"
+	"google.golang.org/grpc"
+)
+
+var (
+	serverAddr = flag.String("server_addr", "localhost:10000", "The server address in the format of host:port")
+)
+
+func printFeature(client pb.RouteGuideClient, point *pb.Point) {
+	log.Printf("Getting feature for point (%d, %d)", point.Latitude, point.Longitude)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	feature, err := client.GetFeature(ctx, point)
+	if err != nil {
+		log.Fatalf("%v.GetFeatures(_) = _, %v: ", client, err)
+	}
+	log.Println(feature)
+}
+
+func printFeatures(client pb.RouteGuideClient, rect *pb.Rectangle) {
+	log.Printf("Looking for features within %v", rect)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	stream, err := client.ListFeatures(ctx, rect)
+	if err != nil {
+		log.Fatalf("%v.ListFeatures(_) = _, %v", client, err)
+	}
+
+	for {
+		feature, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("%v.ListFeatures(_) = _, %v", client, err)
+		}
+
+		log.Printf("Feature: name: %q, point:(%v, %v)", feature.GetName(),
+			feature.GetLocation().GetLatitude(), feature.GetLocation().GetLongitude())
+	}
+}
+
+func main() {
+	var opts []grpc.DialOption
+
+	opts = append(opts, grpc.WithBlock())
+
+	conn, err := grpc.Dial(*serverAddr, opts...)
+
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewRouteGuideClient(conn)
+
+	printFeature(client, &pb.Point{Latitude: 409146138, Longitude: -746188906})
+
+	printFeature(client, &pb.Point{Latitude: 0, Longitude: 0})
+
+	printFeatures(client, &pb.Rectangle{
+		Lo: &pb.Point{Latitude: 400000000, Longitude: -750000000},
+		Hi: &pb.Point{Latitude: 420000000, Longitude: -730000000},
+	})
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module route_guide
+
+go 1.16
+
+require (
+	github.com/bohdanstryber/route_guide v0.0.0-20210709084432-b4f501038a5f // indirect
+	google.golang.org/grpc v1.39.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
